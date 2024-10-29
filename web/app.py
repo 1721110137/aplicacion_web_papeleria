0e4f3b9d7ac0a0b3a1b8ce2650729197806e3aa0
M. Pilar Lopez <1720110203@utectulancingo.edu.mx>
2024-10-29 21:30:37 +0000

    mm

--- web/app.py
+++ web/app.py
@@ -1,7 +1,5 @@
 import web
-import httpx  # Para realizar la solicitud HTTP a la API externa
 
-# URLs de la aplicación
 urls = (
     '/', 'Index',
 )
@@ -10,16 +8,7 @@ app = web.application(urls, globals())
 
 class Index:
     def GET(self):
-        # Hacer la solicitud a la API para obtener los productos
-        try:
-            response = httpx.get("http://localhost:8081/api/productos")
-            productos = response.json()
-        except httpx.HTTPStatusError as e:
-            print(f"Error al hacer la solicitud: {e}")
-            productos = []
-
-        # Pasar los productos a la plantilla index.html
-        return render.index(productos)
+        return render.index() 
 
 if __name__ == "__main__":
     app.run()
